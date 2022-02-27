854e10cc06082c241fd01e290da28a4c28585710
Brian Johnson <johnson8727@gmail.com>
2022-02-26 21:43:07 -0500

    prepared all sass files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <footer class="attribution">
+        Challenge by{" "}
+        <a href="#https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
         </a>
-      </header>
+        . Coded by <a href="#heloo">Your Name Here</a>.
+      </footer>
     </div>
   );
 }
